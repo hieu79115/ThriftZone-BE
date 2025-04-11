231e859b74b9220ed47817f41001e2118cbf07f2
Admin <tptien.a1.c3tqcap@gmail.com>
2025-04-11 21:00:55 +0700

    comming to routing

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -15,9 +15,41 @@ const createOrder= async (req,res)=>{
         res.status(400).json(err);
     }
 }
-
-
-
+const getPaymentCallback = async (req,res)=>{
+    try{
+        const callback= await orderService.processPaymentCallback(
+            req.body.ordered,
+            req.body.paymentResult
+        );
+        res.status(200).json(callback);
+    }catch(err){
+        res.status(400).json(err);
+    }
+}
+const updateShipOrder = async (req,res)=>{
+    try
+    {
+        const callback= await orderService.processPaymentCallback(
+            req.body.orderId,
+            req.body.trackingNumber,
+        );
+        res.status(200).json(callback);
+    }catch(err){
+        res.status(400).json(err);
+    }
+}
+const getOrder = async (req,res)=>{
+    try
+    {
+        const callback= await orderService.getOrderForBuyer(
+            req.body.orderId,
+            req.user._id,
+        );
+        res.status(200).json(callback);
+    }catch(err){
+        res.status(400).json(err);
+    }
+}
 export {
     createOrder
 }
